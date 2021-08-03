e5200db3cb6ee2ea97e3925df16fa1dca6d80cc3
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-03 22:26:07 +0800

    hacker news API service call

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { useEffect, useState } from "react";
+import { getStoryIds } from "./services/hnAPI";
+
 export const App = () => {
-	return <p>Hello!!!</p>;
+	const [storyIds, setStoryIds] = useState([]);
+
+	useEffect(() => {
+		getStoryIds().then((data) => setStoryIds(data));
+	}, []);
+
+	return <p>{storyIds}</p>;
 };
