1e5216091c0a7fa3c1a772e9f0f0b74c3ad4ef74
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-03 22:33:12 +0800

    move the stories from App to StoriesContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import { useEffect, useState } from "react";
-import { getStoryIds } from "./services/hnAPI";
+import { StoriesContainer } from "./containers/StoriesContainer";
 
-export const App = () => {
-	const [storyIds, setStoryIds] = useState([]);
-
-	useEffect(() => {
-		getStoryIds().then((data) => setStoryIds(data));
-	}, []);
-
-	return <p>{storyIds}</p>;
-};
+export const App = () => <StoriesContainer />;
